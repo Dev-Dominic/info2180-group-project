fbe368a5e8e9349620fbb101d34014acaeaab997
Tajcore <45900878+Tajcore@users.noreply.github.com>
2019-11-28 22:20:04 -0500

    Updated HTML CSS JS

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+
+function validatePassword(){
+    var pass = document.getElementById("pword")
+    var strongPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$");
+   
+    if (strongPass.test(pass) == false){
+    pass.setCustomValidity("Password should be at least 8 characters and include 1 digit and uppercase")
+    }
+}
